c336a62aaffb8425f4674343d61fc88571bd1a99
Nelorth <nikolas.kirschstein@gmail.com>
2017-04-24 17:34:50 +0200

    Klasse AbstractView hinzugefügt

---
+++ src/AbstractView.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+/**
+ * Abstrakte Klasse für eine abstrakte Ansicht
+ */
+public abstract class AbstractView extends JPanel {
+    protected MainFrame mainFrame;
+
+    /**
+     * @param mainFrame Das für den weiteren Zugriff benötigte Hauptfenster
+     */
+    public AbstractView(MainFrame mainFrame) {
+        this.mainFrame = mainFrame;
+    }
+}
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,7 +1,23 @@
 import javax.swing.*;
 
 /**
- * Das ist die Hauptfensterklasse
+ * Klasse des Hauptfensters
  */
 public class MainFrame extends JFrame {
+    private AbstractView currentView;
+
+    public MainFrame() {
+        setTitle("PSem Alpha 1.1.2_01");
+        setSize(800, 600);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    /**
+     * Die Hauptmethode der Applikation
+     * @param args Irrelevante Kommandozeilenparamter
+     */
+    public static void main(String[] args) {
+        new MainFrame();
+    }
 }
