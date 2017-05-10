213f23333871bec3130f57e810c9cb424d8d5396
lumpenputzer <benedikt@moedl.org>
2017-05-10 16:28:41 +0200

    Konzeptänderung -Klasse ElementDataHandler entfernt

--- src/util/XmlFileHandler.java
+++ src/util/XmlFileHandler.java
@@ -2,13 +2,9 @@ package util;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
@@ -19,6 +15,12 @@ import javax.xml.xpath.XPathFactory;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+
+/*
+ *Klasse, von der die Element-Daten abgefragt, Elemente hinzugefügt und gelöscht werden können.
+ * Später gibt es schnieke getter-Methoden, die öden XPath-Expressions bekommt außerhalb hier keiner zu Gesicht.
+ * Versprochen.
+ */
 
 public class XmlFileHandler {
     private Document doc; //Document Objekt der zu ladenden Datei
@@ -63,7 +65,7 @@ public class XmlFileHandler {
      * @param myExpr XPath-Pfad
      * @return Nodelist aus allen Elementen mit des Pfades
      */
-    public NodeList getNodeListXPath(String myExpr) throws IOException {
+    private NodeList getNodeListXPath(String myExpr) throws IOException {
         NodeList tNodelist = null;
         XPathFactory xPathfactory = XPathFactory.newInstance();
         XPath xpath = xPathfactory.newXPath();
