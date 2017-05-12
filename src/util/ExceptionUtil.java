36e6502aacb4fae9879aa79e0db23a50b82635b5
Nelorth <nikolas.kirschstein@gmail.com>
2017-05-12 22:07:37 +0200

    Kellog's logging!

---
+++ src/util/ExceptionUtil.java
@@ -0,0 +1,42 @@
+package util;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ExceptionUtil {
+    private static BufferedWriter writer;
+
+    public static void log(Exception exception) {
+        if (writer == null) {
+            try {
+                 writer = new BufferedWriter(new FileWriter("log.txt", true));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            for (StackTraceElement s : exception.getStackTrace()) {
+                writer.write("Exception: " + exception.getMessage());
+                writer.newLine();
+                writer.write("File: " + s.getFileName());
+                writer.newLine();
+                writer.write("Class: " + s.getClassName());
+                writer.newLine();
+                writer.write("Method: " + s.getMethodName());
+                writer.newLine();
+                writer.write("Line: " + s.getLineNumber());
+                writer.newLine();
+                writer.newLine();
+            }
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        RuntimeException e = new RuntimeException("Du bist böse");
+        log(e);
+    }
+}
