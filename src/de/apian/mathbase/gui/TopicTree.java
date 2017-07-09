f1cc2fef4b1854561c13d92cb72dab5de7143f01
Nelorth <nikolas.kirschstein@gmail.com>
2017-07-09 17:58:39 +0200

    Musterschutz muss sein :D

--- src/de/apian/mathbase/gui/TopicTree.java
+++ src/de/apian/mathbase/gui/TopicTree.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 MathBox P-Seminar 16/18. All rights reserved.
+ * This product is licensed under the GNU General Public License v3.0.
+ * See LICENSE file for further information.
+ */
+
 package de.apian.mathbase.gui;
 
 import javafx.scene.Parent;
