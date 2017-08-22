8c7956ed5c9f68aa01a2f26e525e2fdab375ee97
lumpenputzer <benedikt@moedl.org>
2017-08-22 20:08:28 +0200

    JAAAAA

--- src/de/apian/mathbase/gui/Mathbase.java
+++ src/de/apian/mathbase/gui/Mathbase.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2017 MathBox P-Seminar 16/18. All rights reserved.
  * This product is licensed under the GNU General Public License v3.0.
- * See LICENSE file for further information
+ * See LICENSE file for further information.
  */
 
 package de.apian.mathbase.gui;
