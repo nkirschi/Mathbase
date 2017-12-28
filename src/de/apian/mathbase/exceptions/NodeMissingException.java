7a4d98b2f0249113bdbb0997931540034a07f531
lumpenputzer <dev@benedikt.moedl.org>
2017-12-28 11:50:15 +0100

    Ausnahmen

---
+++ src/de/apian/mathbase/exceptions/NodeMissingException.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2017. MathBox P-Seminar 16/18. All rights reserved.
+ * This product is licensed under the GNU General Public License v3.0.
+ * See LICENSE file for further information.
+ */
+
+package de.apian.mathbase.exceptions;
+
+/**
+ * Wird geworfen, falls es zu einer Kollision der Titel zweier Knoten kommen würde
+ *
+ * @since 1.0
+ */
+public class NodeMissingException extends Throwable {
+}
