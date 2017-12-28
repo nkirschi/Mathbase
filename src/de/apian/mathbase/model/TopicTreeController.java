2219434d2394292ba4cc0c8b11c8b8d94070b03e
lumpenputzer <dev@benedikt.moedl.org>
2017-12-28 13:23:09 +0100

    Inhalte

--- src/de/apian/mathbase/model/TopicTreeController.java
+++ src/de/apian/mathbase/model/TopicTreeController.java
@@ -1,13 +1,15 @@
 /*
- * Copyright (c) 2017 MathBox P-Seminar 16/18. All rights reserved.
+ * Copyright (c) 2017. MathBox P-Seminar 16/18. All rights reserved.
  * This product is licensed under the GNU General Public License v3.0.
  * See LICENSE file for further information.
  */
 
-package de.apian.mathbase.util;
+package de.apian.mathbase.model;
 
 import de.apian.mathbase.exceptions.NodeMissingException;
 import de.apian.mathbase.exceptions.TitleCollisionException;
+import de.apian.mathbase.util.Constants;
+import de.apian.mathbase.util.Logger;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -106,10 +108,21 @@ public class TopicTreeController {
     /**
      * Konstruktor
      *
-     * @throws IOException wenn die Datei sowie die Backup-Datei nicht geladen werden konnten
+     * @throws IOException wenn das Laden der XML-Datei fehlschlägt
      * @since 1.0
      */
     public TopicTreeController() throws IOException {
+        load();
+    }
+
+    /**
+     * Lädt die XML-Datei. Ist dies nicht möglich wird die Backupdatei geladen, ist dies auch nicht möglich bricht die
+     * Methode ab
+     *
+     * @throws IOException wenn die Datei sowie die Backup-Datei nicht geladen werden konnten
+     * @since 1.0
+     */
+    private void load() throws IOException {
         try { // Versuche zuerst die Original-Datei zu laden
             xmlHandler = new XMLFileHandler(ORIGINAL_PATH);
             Logger.log(Level.INFO, "Original-Datei \"" + ORIGINAL_PATH + "\" erfolgreich geladen");
@@ -126,8 +139,7 @@ public class TopicTreeController {
                         + BACKUP_PATH + "\" wiederhergestellt werden", ex2);
 
                 // Schmeiß eine IOException, um den aufrufenden Klassen mitzuteilen,
-                // dass die Datei nicht geladen werden konnte; diese sollen dann weiter verfahren!
-                // TODO Errorhandling in den anderen Klassen implementieren
+                // dass die Datei nicht geladen werden konnte
                 throw new IOException("Keine Datei konnte geladen werden!");
             }
         }
@@ -135,7 +147,7 @@ public class TopicTreeController {
 
     /**
      * Speichern der Daten als XML-Datei im Pfad {@value #ORIGINAL_PATH} relativ zum Arbeitsverzeichnis.
-     * Wird von den die XML-Datei bearbeitenden Methoden (welche sich nur in dieser Klasse befinden) selbst aufgerufen.
+     * Wird von den die XML-Datei bearbeitenden Methoden selbst aufgerufen.
      *
      * @throws IOException wenn das Speichern nicht erfolgreich war
      * @since 1.0
@@ -147,7 +159,7 @@ public class TopicTreeController {
         } catch (IOException ex) {
             Logger.log(Level.WARNING, "Fehler beim Speichern von \"" + ORIGINAL_PATH + "\"", ex);
             // Schmeiß eine IOException, um den aufrufenden Methoden mitzuteilen,
-            // dass die Datei nicht geladen werden konnte; diese sollen dann weiter verfahren!
+            // dass die Datei nicht gespeichert werden konnte
             throw ex;
         }
     }
@@ -255,19 +267,28 @@ public class TopicTreeController {
     }
 
     /**
-     * Gibt eine {@code NodeList} aller direkten Kind-Knoten eines bestimmten Knotens zurück
+     * Gibt eine {@code String}-Array der Titel aller direkten Kind-Knoten eines bestimmten Knotens zurück
      *
      * @param title Der Titel des Knotens
-     * @return Eine {@code Nodelist} aller direkten Kind-Knoten des Knotens
+     * @return Ein {@code String}-Array der Titel aller direkten Kind-Knoten des Knotens
      * @since 1.0
      */
-    public NodeList getChildNodes(String title) {
+    public String[] getChildNodes(String title) {
         try {
             NodeList nodeList = xmlHandler.getNodeListXPath("//" + TAG_NODE + "[@" + ATTR_TITLE + "='" + title
                     + "']/" + TAG_NODE);
-            Logger.log(Level.INFO, "Kind-Knoten des Knotens mit dem Titel \"" + title + "\" zurückgegeben");
-            return nodeList;
-        } catch (XPathExpressionException e) { // Kann eigentlich niemals vorkommen
+            String[] result = new String[nodeList.getLength()];
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                result[i] = nodeList.item(i).getAttributes().getNamedItem(ATTR_TITLE).getTextContent();
+            }
+            Logger.log(Level.INFO, "Titel aller Kind-Knoten des Knotens mit dem Titel \"" + title + "\" zurückgegeben");
+            return result;
+        } catch (XPathExpressionException e) {
+            /*
+             * Dieser Fall kann eigentlich niemals eintreten, da die XPathExpression hardgecoded ist.
+             * Sollte unwahrscheinlicherweise doch einmal etwas an der XPath-API geändert werden,
+             * wäre das gesamte Programm sowieso erstmal unbrauchbar!
+             */
             Logger.log(Level.WARNING, Constants.FATAL_ERROR_MESSAGE, e);
             throw new InternalError(Constants.FATAL_ERROR_MESSAGE);
         }
@@ -282,7 +303,7 @@ public class TopicTreeController {
     public String[] getTopNodes() {
         try {
             NodeList nodeList = xmlHandler.getNodeListXPath("//" + TAG_ROOT + "/" + TAG_NODE);
-            String result[] = new String[nodeList.getLength()];
+            String[] result = new String[nodeList.getLength()];
             for (int i = 0; i < nodeList.getLength(); i++) {
                 result[i] = nodeList.item(i).getAttributes().getNamedItem(ATTR_TITLE).getTextContent();
             }
@@ -304,8 +325,12 @@ public class TopicTreeController {
      *
      * @param parent Titel des Eltern-Knotens
      * @param title  Titel des Knotens
+     * @throws TitleCollisionException wenn bereits ein Knoten mit diesem Titel existiert
+     * @throws NodeMissingException    wenn der Elternknoten nicht exisiert
+     * @throws IOException             wenn speichern der XML-Datei fehlschlägt
+     * @since 1.0
      */
-    public void createNode(String parent, String title) throws TitleCollisionException, NodeMissingException {
+    public void createNode(String parent, String title) throws TitleCollisionException, NodeMissingException, IOException {
         if (alreadyExists(title))
             throw new TitleCollisionException();
 
@@ -315,8 +340,6 @@ public class TopicTreeController {
                     + "']");
             if (nodeList.getLength() == 0)
                 throw new NodeMissingException();
-            else if (nodeList.getLength() > 1)
-                throw new TitleCollisionException();
             Node parentNode = nodeList.item(0);
             Element element = xmlHandler.getDoc().createElement(TAG_NODE);
             element.setAttribute(ATTR_TITLE, title);
@@ -324,7 +347,9 @@ public class TopicTreeController {
 
             //Ordner wird erstellt
 
+
             //XML-Datei wird gespeichert
+            save();
         } catch (XPathExpressionException e) {
             /*
              * Dieser Fall kann eigentlich niemals eintreten, da die XPathExpression hardgecoded ist.
