b4694714c087de3026bb864a9b21a9fcdbddfbd0
lumpenputzer <dev@benedikt.moedl.org>
2018-01-06 15:54:04 +0100

    Das hätte schiefgehen können

--- src/main/java/de/apian/mathbase/xml/Content.java
+++ src/main/java/de/apian/mathbase/xml/Content.java
@@ -52,7 +52,8 @@ public class Content {
     public Content(Type type, String path, @Nullable String title) {
         this.type = type;
         this.path = path;
-        this.title = title;
+        this.title = title == null ? "" : title; // Titel sollte nicht NULL sein, sonst bekommen wir beim Speichern in
+        // der XML ein Problem
     }
 
     /**
