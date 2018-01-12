988aff998b8fc034e4a6f3dbaad0030a66bb5bd1
Slaergh <nikolas.kirschstein@gmail.com>
2018-01-12 19:24:58 +0100

    Weiteres

--- src/main/java/de/apian/mathbase/gui/content/AddContentDialog.java
+++ src/main/java/de/apian/mathbase/gui/content/AddContentDialog.java
@@ -9,10 +9,13 @@ package de.apian.mathbase.gui.content;
 import de.apian.mathbase.util.Constants;
 import de.apian.mathbase.util.Images;
 import de.apian.mathbase.xml.Content;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
+import javafx.collections.FXCollections;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.stage.Window;
+import javafx.util.Callback;
+import javafx.util.StringConverter;
 
 /**
  * Dialog zum Hinzufügen eines Inhalts
@@ -29,6 +32,22 @@ public class AddContentDialog extends Dialog<Content> {
         setHeaderText(Constants.BUNDLE.getString("add_content"));
         setGraphic(new ImageView(Images.getInternal("icons_x48/multimedia.png")));
         setResizable(true);
+
+
+        ComboBox<Content.Type> comboBox = new ComboBox<>(FXCollections.observableArrayList(Content.Type.values()));
+        comboBox.setPromptText("Bitte auswählen...");
+        comboBox.setConverter(new StringConverter<Content.Type>() {
+            @Override
+            public String toString(Content.Type object) {
+                return Constants.BUNDLE.getString(object.toString());
+            }
+
+            @Override
+            public Content.Type fromString(String string) {
+                return null;
+            }
+        });
+        getDialogPane().setContent(new HBox(comboBox));
 
         getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
         setResultConverter(buttonType -> {
